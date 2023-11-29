50a1b2f755a973b9fe0207e66757db49a0496be6
Matias <mapresta00@gmail.com>
2023-11-29 11:07:21 -0300

    Updates
    Mejoras en el código y corrección de errores

--- validacion.js
+++ validacion.js
@@ -1,33 +1,32 @@
-const btnEnviar = document.getElementById('btn-enviar');
+const btnEnviar = document.getElementById("btn-enviar");
 
 //Validamos los ingresos
 function validacion() {
+  const nombre = document.getElementById("nombre");
+  const asunto = document.getElementById("asunto");
+  const mensaje = document.getElementById("mensaje");
+  console.log(nombre, asunto, mensaje);
 
-    const nombre = document.getElementById('nombre');
-    const asunto = document.getElementById('asunto');
-    const mensaje = document.getElementById('mensaje');
-    console.log(nombre, asunto, mensaje);
-    
-    if (nombre.value === "" || nombre.value === " ") {
-      alert("Por favor, escribe tu nombre");
-      nombre.focus();
-      return false;
-    }
-      
-    if (asunto.value === ""|| asunto.value === " ") {
-      alert("Por favor, escribe un asunto válido");
-      asunto.focus();
-      return false;
-    }
-  
-    if (mensaje.value === ""|| mensaje.value === " ") {
-      alert("Por favor, escribe un mensaje válido");
-      mensaje.focus();
-      return false;
-    }
-    
-    console.log(nombre, asunto, mensaje);
-    return true; //Se pueden enviar los datos del formulario al servidor
+  if (nombre.value === "" || nombre.value === " ") {
+    alert("Por favor, escribe tu nombre");
+    nombre.focus();
+    return false;
   }
-  
-btnEnviar.addEventListener('click', validacion);
+
+  if (asunto.value === "" || asunto.value === " ") {
+    alert("Por favor, escribe un asunto válido");
+    asunto.focus();
+    return false;
+  }
+
+  if (mensaje.value === "" || mensaje.value === " ") {
+    alert("Por favor, escribe un mensaje válido");
+    mensaje.focus();
+    return false;
+  }
+
+  console.log(nombre, asunto, mensaje);
+  return true; //Se pueden enviar los datos del formulario al servidor
+}
+
+btnEnviar.addEventListener("click", validacion);
